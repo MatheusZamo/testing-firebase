7140bb3108691079c65595b737ce0b7e1ead1f7f
Matheus Henrique <19matheushenrique03@gmail.com>
2024-06-18 18:59:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,69 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js'
-import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js'
+import { getFirestore, collection, getDocs, addDoc, serverTimestamp, doc, deleteDoc, onSnapshot } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js'
 
-  const firebaseConfig = {
-    apiKey: 'AIzaSyCbCND3A5RFLNPa6tt05hp-j7oLJn-mA7E',
-    authDomain: 'testing-firebase-d4355.firebaseapp.com',
-    projectId: 'testing-firebase-d4355',
-    storageBucket: 'testing-firebase-d4355.appspot.com',
-    messagingSenderId: '121381485430',
-    appId: '1:121381485430:web:b7999983c4546c1f645857',
-    measurementId: 'G-F9H2J6JLSQ'
-  };
+const firebaseConfig = {
+  apiKey: 'AIzaSyCbCND3A5RFLNPa6tt05hp-j7oLJn-mA7E',
+  authDomain: 'testing-firebase-d4355.firebaseapp.com',
+  projectId: 'testing-firebase-d4355',
+  storageBucket: 'testing-firebase-d4355.appspot.com',
+  messagingSenderId: '121381485430',
+  appId: '1:121381485430:web:b7999983c4546c1f645857',
+  measurementId: 'G-F9H2J6JLSQ'
+};
 
   
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const collectionGames = collection(db, 'games')
+
+const formAddGame = document.querySelector('[data-js="add-game-form"]')
+const gamesList = document.querySelector('[data-js="games-list"]')
+const buttonUnsub = document.querySelector('[data-js="unsub"]')
+
+const unsubscribe = onSnapshot(collectionGames, querySnapshot => {
+  if(!querySnapshot.metadata.hasPendingWrites) {
+    const gamesLis = querySnapshot.docs.reduce((acc, doc) => {
+      const { title, developedBy, createdAt } = doc.data()
+
+      acc += `<li class="my-4" data-id ="${doc.id}">
+      <h5>${title}</h5>
+          
+      <ul>
+        <li>Desenvolvido por ${developedBy}</li>
+        <li>Adicionado no banco em ${createdAt.toDate()}</li>
+      </ul>
+      <button class="btn btn-danger btn-sm" data-remove="${doc.id}">Remover</button>
+    </li>`
+
+    return acc
+  }, '')
+
+  gamesList.innerHTML = gamesLis
+  }
+})
+
+
+formAddGame.addEventListener('submit', e =>{
+  e.preventDefault()
   
-  getDocs(collection(db, 'games'))
-    .then(querySnapshot => {
-        const gamesLis = querySnapshot.docs.reduce((acc, doc) => {
-            const { title, developedBy, createdAt } = doc.data()
+  addDoc(collectionGames, {
+    title : e.target.title.value,
+    developedBy : e.target.developer.value,
+    createdAt : serverTimestamp()
+  })
+  .then(doc => console.log("Documento criado com o Id", doc.id))
+  //.cath(console.log)
+})
 
-            acc += `<li class="my-4">
-            <h5>${title}</h5>
-            
-            <ul>
-              <li>Desenvolvido por ${developedBy}</li>
-              <li>Adicionado no banco em ${createdAt.toDate()}</li>
-            </ul>
-          </li>`
+gamesList.addEventListener('click',e => {
+  const idRemoveButton = e.target.dataset.remove
+  
+  if(idRemoveButton){
+    deleteDoc(doc(db, 'games', idRemoveButton))
+      .then(() => console.log('Game removido'))
+      .catch(console.log)
+    
+  }
+})
 
-          return acc
-        }, '')
-
-        const gamesList = document.querySelector('[data-js="games-list"]')
-        gamesList.innerHTML += gamesLis
-    }) 
-    .cath(console.log('Error'))
+buttonUnsub.addEventListener('click',unsubscribe)
